6c2250069cea09ca0694e40ba42d4a4ea48232d2
ragnarokio <46255208+ragnarokio@users.noreply.github.com>
2018-12-30 00:28:49 -0500

    up

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+var food = 0;
+var wood = 0;
 
+function gatherFood(){
+    food = food + 5;
+    document.getElementById("food").innerHTML = food;
+};
+
+function gatherWood(){
+    wood = wood + 1;
+    document.getElementById("wood").innerHTML = wood;
+};
