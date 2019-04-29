151d190f69d62b275114ac33247b5771cd11e0d6
ragnarokio <46255208+ragnarokio@users.noreply.github.com>
2019-04-29 10:07:17 -0400

    Update f.js

--- f.js
+++ f.js
@@ -1,9 +1,9 @@
 class Drawing {
 	constructor(x,y,img,name){
-		this.x = x,
-		this.y = y,
-		this.img = img,
-		this.name = name
+		this.x = x;
+		this.y = y;
+		this.img = img;
+		this.name = name;
 	}
 }
 
